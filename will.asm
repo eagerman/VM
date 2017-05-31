8dc34c567587c1c1cdcf67b1801f22a9878b3108
wilzlerr <william.weng@hotmail.com>
2017-05-31 18:49:23 +1000

    adding key check code

--- will.asm
+++ will.asm
@@ -8,12 +8,13 @@
 .include "m2560def.inc"
 
 ;========== GLOBAL VARIABLE ========== GLOBAL VARIABLE ========== GLOBAL VARIABLE ========== GLOBAL VARIABLE ========== GLOBAL VARIABLE ==========
-.equ    ITEM_STRUCT_SIZE = 18
+.equ	ITEM_STRUCT_SIZE = 18
 
 .dseg
-    item_struct:    .byte ITEM_STRUCT_SIZE
+	item_struct:	.byte ITEM_STRUCT_SIZE
 
 .def temp = r16
+.def key = r26 ;holds the latest key pressed
 
 ; The macro clears a word (2 bytes) in a memory
 ; the parameter @0 is the memory address for that word
@@ -35,13 +36,13 @@
 .equ LCD_RW = 5
 .equ LCD_BE = 4
 
-.macro do_lcd_command   ;send a command to the LCD
+.macro do_lcd_command	;send a command to the LCD
         ldi lcd, @0
         rcall lcd_command
         rcall lcd_wait
 .endmacro
 
-.macro do_lcd_data      ; send Data to display
+.macro do_lcd_data		; send Data to display
         ldi lcd, @0
         rcall lcd_data
         rcall lcd_wait
@@ -57,17 +58,33 @@
 
 ;========== LCD MACROS ========== LCD MACROS ========== LCD MACROS ========== LCD MACROS ========== LCD MACROS ========== 
 
+;========== KEYPAD MACROS ========== KEYPAD MACROS ========== KEYPAD MACROS ========== KEYPAD MACROS ========== KEYPAD MACROS ==========
+
+.def row = r16              ; current row number
+.def col = r17              ; current column number
+.def rmask = r18            ; mask for current row during scan
+.def cmask = r19            ; mask for current column during scan
+.def temp1 = r20 
+.def temp2 = r21
+
+.equ PORTLDIR = 0xF0        ; PH7-4: output, PH3-0, input
+.equ INITCOLMASK = 0xEF     ; scan from the rightmost column,
+.equ INITROWMASK = 0x01     ; scan from the top row
+.equ ROWMASK = 0x0F         ; for obtaining input from Port L
+
+;========== KEYPAD MACROS ========== KEYPAD MACROS ========== KEYPAD MACROS ========== KEYPAD MACROS ========== KEYPAD MACROS ==========
+
 ;========== SETUP MACROS ========== SETUP MACROS ========== SETUP MACROS ========== SETUP MACROS ========== SETUP MACROS ==========
 .macro isOdd
-    .if @1 & 1
-        st @0, 1
-    .else
-        st @0, 2
-    .endif
+	.if @1 & 1
+		st @0, 1
+	.else
+		st @0, 2
+	.endif
 .endmacro
 
 .macro fillElement
-    
+	
 .endmacro
 ;========== SETUP MACROS ========== SETUP MACROS ========== SETUP MACROS ========== SETUP MACROS ========== SETUP MACROS ========== 
 
@@ -94,225 +111,236 @@ DEFAULT:  reti          ; no service
 
 ;========== RESET ========== RESET ==========  RESET ==========  RESET ==========  RESET ========== 
 RESET:
-    ;stack pointer initialisation
-    ldi temp, low(RAMEND)
-    out SPL, temp
-    ldi temp, high(RAMEND)
-    out SPH, temp
-    sei
-    
-    ; LCD setup
-    ser temp
-    out DDRF, temp ; data input address
-    out DDRA, temp ; inst inpit address
-    clr temp
-    out PORTF, temp; data port set
-    out PORTA, temp; inst port set
+	;stack pointer initialisation
+	ldi temp, low(RAMEND)
+	out SPL, temp
+	ldi temp, high(RAMEND)
+	out SPH, temp
+	sei
+	
+	; LCD setup
+	ser temp
+	out DDRF, temp ; data input address
+	out DDRA, temp ; inst inpit address
+	clr temp
+	out PORTF, temp; data port set
+	out PORTA, temp; inst port set
 
-    ; LCD: init the settings
-    do_lcd_command 0b00111000 ; 2x5x7
-    rcall sleep_5ms
-    do_lcd_command 0b00111000 ; 2x5x7
-    rcall sleep_1ms
-    do_lcd_command 0b00111000 ; 2x5x7
-    do_lcd_command 0b00111000 ; 2x5x7
-    do_lcd_command 0b00001000 ; display off?
-    do_lcd_command 0b00000001 ; clear display
-    do_lcd_command 0b00000110 ; increment, no display shift
-    do_lcd_command 0b00001100 ; Cursor on, bar, no blink
+	; LCD: init the settings
+	do_lcd_command 0b00111000 ; 2x5x7
+	rcall sleep_5ms
+	do_lcd_command 0b00111000 ; 2x5x7
+	rcall sleep_1ms
+	do_lcd_command 0b00111000 ; 2x5x7
+	do_lcd_command 0b00111000 ; 2x5x7
+	do_lcd_command 0b00001000 ; display off?
+	do_lcd_command 0b00000001 ; clear display
+	do_lcd_command 0b00000110 ; increment, no display shift
+	do_lcd_command 0b00001100 ; Cursor on, bar, no blink
 
-    ldi temp, 1
-    rjmp start
+	ldi temp, 1
+	rjmp start
 ;========== RESET ========== RESET ==========  RESET ==========  RESET ==========  RESET ========== 
 
 ;========== INTERUPTS ==========  INTERUPTS ==========  INTERUPTS ==========  INTERUPTS ========== 
 Timer0OVF: ; interrupt subroutine to Timer0
-    push r24
-    push r25
+	push r24
+	push r25
 
 counting:
-    lds r24, Timer1Counter
+	lds r24, Timer1Counter
     lds r25, Timer1Counter+1
-    adiw r25:r24, 1 ; Increase the temporary counter by one.
-    sts Timer1Counter, r24
+	adiw r25:r24, 1 ; Increase the temporary counter by one.
+	sts Timer1Counter, r24
     sts Timer1Counter+1, r25
 
 endTimer:
-    pop r25
-    pop r24
-    reti
+	pop r25
+	pop r24
+	reti
 
 ;========== INTERUPTS ==========  INTERUPTS ==========  INTERUPTS ==========  INTERUPTS ========== 
 
 ;========== START ========== START ========== START ========== START ========== START ==========
 start:
-    ldi temp, 0b00000000
+	ldi temp, 0b00000000
     out TCCR0A, temp
     ldi temp, 0b00000010
     out TCCR0B, temp        ; Prescaling value = 8
     ldi temp, 1<<TOIE0      ; = 128 microseconds
     sts TIMSK0, temp        ; T/C0 interrupt enable
 
-    rcall fill_struct
-    rcall initial_screen
+	ldi key, 'f'
+	rcall fill_struct
+	rcall initial_screen
     clear Timer1Counter       ; Initialize the temporary counter to 0
-    clr temp
+	clr temp
 
 startLoop:
-    rcall check3
-    cpi temp, 1
-    breq selectScreen
-    rjmp startLoop
-    
+	rcall check3
+	cpi temp, 1
+	breq selectScreen
+	rcall checkKey
+	cpi key, 'f'
+	brne selectScreen
+	rjmp startLoop
+	
 ;========== START ========== START ========== START ========== START ========== START ==========
 
 ;========== SELECTIONSCREEN ========== SELECTIONSCREEN ========== SELECTIONSCREEN ========== SELECTIONSCREEN ========== SELECTIONSCREEN ========== 
 selectScreen:
-    do_lcd_command 0b00000001 ; clear display
-    do_lcd_data 'S'
-    do_lcd_data 'e'
-    do_lcd_data 'l'
-    do_lcd_data 'e'
-    do_lcd_data 'c'
-    do_lcd_data 't'
-    do_lcd_data ' '
-    do_lcd_data 'i'
-    do_lcd_data 't'
-    do_lcd_data 'e'
-    do_lcd_data 'm'
+	do_lcd_command 0b00000001 ; clear display
+	do_lcd_data 'S'
+	do_lcd_data 'e'
+	do_lcd_data 'l'
+	do_lcd_data 'e'
+	do_lcd_data 'c'
+	do_lcd_data 't'
+	do_lcd_data ' '
+	do_lcd_data 'i'
+	do_lcd_data 't'
+	do_lcd_data 'e'
+	do_lcd_data 'm'
+
+	
 ;========== SELECTIONSCREEN ========== SELECTIONSCREEN ========== SELECTIONSCREEN ========== SELECTIONSCREEN ========== SELECTIONSCREEN ========== 
 
 end:
-    rjmp end
+	rjmp end
 
 ;========== HELPER FUNCTIONS ========== HELPER FUNCTIONS ========== HELPER FUNCTIONS ========== HELPER FUNCTIONS ========== HELPER FUNCTIONS ==========
 ;fill struct function
 fill_struct: 
-    push YH
-    push YL
-    push r16
-    ;push r17
+	push YH
+	push YL
+	push r16
+	;push r17
 
-    ldi YH, high(item_struct)
-    ldi YL, low(item_struct)
-    ;ldi r16, 1
-    ;clr r17
+	ldi YH, high(item_struct)
+	ldi YL, low(item_struct)
+	;ldi r16, 1
+	;clr r17
 
 /*
 fill:
-    cpi r16, 10
-    breq end_fill
-    st Y+, r16
-    isOdd r17, r16
-    st Y+, r17
-    inc r16
-    rjmp fill
+	cpi r16, 10
+	breq end_fill
+	st Y+, r16
+	isOdd r17, r16
+	st Y+, r17
+	inc r16
+	rjmp fill
 */
 
 fill:
-    ldi r16, 1
-    st Y+, r16
-    ldi r16, 1
-    st Y+, r16
-    ldi r16, 1
-    st Y+, r16
-    ldi r16, 2
-    st Y+, r16
-    ldi r16, 3
-    st Y+, r16
-    ldi r16, 1
-    st Y+, r16
-    ldi r16, 4
-    st Y+, r16
-    ldi r16, 2
-    st Y+, r16
-    ldi r16, 5
-    st Y+, r16
-    ldi r16, 1
-    st Y+, r16
-    ldi r16, 6
-    st Y+, r16
-    ldi r16, 2
-    st Y+, r16
-    ldi r16, 7
-    st Y+, r16
-    ldi r16, 1
-    st Y+, r16
-    ldi r16, 8
-    st Y+, r16
-    ldi r16, 2
-    st Y+, r16
-    ldi r16, 9
-    st Y+, r16
-    ldi r16, 1
-    st Y, r16
+	ldi r16, 1
+	st Y+, r16
+	ldi r16, 1
+	st Y+, r16
+	ldi r16, 1
+	st Y+, r16
+	ldi r16, 2
+	st Y+, r16
+	ldi r16, 3
+	st Y+, r16
+	ldi r16, 1
+	st Y+, r16
+	ldi r16, 4
+	st Y+, r16
+	ldi r16, 2
+	st Y+, r16
+	ldi r16, 5
+	st Y+, r16
+	ldi r16, 1
+	st Y+, r16
+	ldi r16, 6
+	st Y+, r16
+	ldi r16, 2
+	st Y+, r16
+	ldi r16, 7
+	st Y+, r16
+	ldi r16, 1
+	st Y+, r16
+	ldi r16, 8
+	st Y+, r16
+	ldi r16, 2
+	st Y+, r16
+	ldi r16, 9
+	st Y+, r16
+	ldi r16, 1
+	st Y, r16
 
 end_fill:
-;   pop r17
-    pop r16
-    pop YL
-    pop YH
-    ret
-    
+;	pop r17
+	pop r16
+	pop YL
+	pop YH
+	ret
+	
 ;Inital screen function
 initial_screen:
-    do_lcd_command 0b00000001 ; clear display
-    do_lcd_data '2'
-    do_lcd_data '1'
-    do_lcd_data '2'
-    do_lcd_data '1'
-    do_lcd_data ' '
-    do_lcd_data '1'
-    do_lcd_data '7'
-    do_lcd_data 's'
-    do_lcd_data '1'
-    do_lcd_data ' '
-    do_lcd_data ' '
-    do_lcd_data ' '
-    do_lcd_data 'L'
-    do_lcd_data '1'
-    do_lcd_data '2'
-    do_lcd_command 0b11000000
-    do_lcd_data 'V'
-    do_lcd_data 'e'
-    do_lcd_data 'n'
-    do_lcd_data 'd'
-    do_lcd_data 'i'
-    do_lcd_data 'n'
-    do_lcd_data 'g'
-    do_lcd_data ' '
-    do_lcd_data 'M'
-    do_lcd_data 'a'
-    do_lcd_data 'c'
-    do_lcd_data 'h'
-    do_lcd_data 'i'
-    do_lcd_data 'n'
-    do_lcd_data 'e'
+	do_lcd_command 0b00000001 ; clear display
+	do_lcd_data '2'
+	do_lcd_data '1'
+	do_lcd_data '2'
+	do_lcd_data '1'
+	do_lcd_data ' '
+	do_lcd_data '1'
+	do_lcd_data '7'
+	do_lcd_data 's'
+	do_lcd_data '1'
+	do_lcd_data ' '
+	do_lcd_data ' '
+	do_lcd_data ' '
+	do_lcd_data 'L'
+	do_lcd_data '1'
+	do_lcd_data '2'
+	do_lcd_command 0b11000000
+	do_lcd_data 'V'
+	do_lcd_data 'e'
+	do_lcd_data 'n'
+	do_lcd_data 'd'
+	do_lcd_data 'i'
+	do_lcd_data 'n'
+	do_lcd_data 'g'
+	do_lcd_data ' '
+	do_lcd_data 'M'
+	do_lcd_data 'a'
+	do_lcd_data 'c'
+	do_lcd_data 'h'
+	do_lcd_data 'i'
+	do_lcd_data 'n'
+	do_lcd_data 'e'
 ret
 
 check3:
-    push r24
-    push r25
+	push r24
+	push r25
 
 checking:
-    lds r24, Timer1Counter
+	lds r24, Timer1Counter
     lds r25, Timer1Counter+1
 
-    cpi r24, low(23436)      ; 1953 is what we need Check if (r25:r24) = 7812 ; 7812 = 10^6/128
+	cpi r24, low(23436)      ; 1953 is what we need Check if (r25:r24) = 7812 ; 7812 = 10^6/128
     ldi temp, high(23436)    ; 7812 interrupts = 1 second, 3906 interrupts = 0.5 seconds
     cpc r25, temp
-    breq is3
-    ldi temp, 0
-    pop r25
-    pop r24
-    ret
+	breq is3
+	ldi temp, 0
+	pop r25
+	pop r24
+	ret
 
 is3:
-    ldi temp, 1
-    pop r25
-    pop r24
-    ret
+	ldi temp, 1
+	pop r25
+	pop r24
+	ret
 ;========== HELPER FUNCTIONS ========== HELPER FUNCTIONS ========== HELPER FUNCTIONS ========== HELPER FUNCTIONS ========== HELPER FUNCTIONS ==========
+
+;========== KEYPAD FUNCTIONS ========== KEYPAD FUNCTIONS ========== KEYPAD FUNCTIONS ========== KEYPAD FUNCTIONS ========== KEYPAD FUNCTIONS ========== 
+checkKey:
+
+;========== KEYPAD FUNCTIONS ========== KEYPAD FUNCTIONS ========== KEYPAD FUNCTIONS ========== KEYPAD FUNCTIONS ========== KEYPAD FUNCTIONS ========== 
 
 ;========== LCD HELPER FUNCTION ========== LCD HELPER FUNCTION ========== LCD HELPER FUNCTION ========== LCD HELPER FUNCTION ========== LCD HELPER FUNCTION ========== 
 
@@ -323,7 +351,7 @@ is3:
 lcd_command:
         out PORTF, lcd
         rcall sleep_1ms
-        lcd_set LCD_E   ; turn on the enable pin
+        lcd_set LCD_E	; turn on the enable pin
         rcall sleep_1ms
         lcd_clr LCD_E
         rcall sleep_1ms
